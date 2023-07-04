6c4fd6568a093d39fc6cdc74a5535d4311c37d35
floidenergy <manipulatorrika@gmail.com>
2023-07-04 21:21:37 +0100

    trying to fix production cookies x2

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -3,7 +3,6 @@
 import React, { useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import Cookie from 'js-cookie'
 import { useDispatch, useSelector } from 'react-redux'
 import {login} from '../../store/features/user'
 
